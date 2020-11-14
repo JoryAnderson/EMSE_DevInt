17cc1b31ee24185988f241ed8d809f0bce1397a7
Jory Anderson <Jory2554@gmail.com>
2020-11-13 18:12:33 -0800

    Drafted APIDump. Edit constants, then run from interpreter. Will create JSON files.

---
+++ python/APIDump/APIDump.py
@@ -0,0 +1,41 @@
+import time
+from stackapi import StackAPI
+import json
+
+QUERY_SIZE = 3
+NUM_OF_QUERIES = 1
+
+
+def get_posts():
+    SITE = StackAPI('stackoverflow')
+
+    # Adjust returning data
+    SITE.page_size = QUERY_SIZE
+    SITE.max_pages = NUM_OF_QUERIES
+
+    # Get Data
+    posts = SITE.fetch('posts')
+
+    return posts
+
+
+def convert_dict_to_json(posts):
+    return json.dumps(posts)
+
+
+def convert_json_to_dict(json):
+    return json.loads(json)
+
+
+def export_json_to_file(posts):
+    data = json.dumps(posts, indent=4)
+
+    epoch_time = int(time.time())
+    f = open("data" + str(epoch_time) + ".json", "w")
+    f.write(data)
+    f.close()
+
+
+if __name__ == '__main__':
+    posts = get_posts()
+    export_json_to_file(posts)
