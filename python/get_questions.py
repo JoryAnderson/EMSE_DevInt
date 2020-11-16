107397193070d31881fdb454f9d7dda097b59aae
Jory Anderson <Jory2554@gmail.com>
2020-11-16 05:15:26 -0800

    Fixed incorrect reference module Formatted to PEP

--- python/get_questions.py
+++ python/get_questions.py
@@ -1,7 +1,6 @@
 from APIDump import APIDump
-from APIDump import JSONReader
 
 if __name__ == '__main__':
     SITE = APIDump.config_api(100, 250)
     posts = APIDump.get_questions(SITE)
-    JSONReader.export_json_to_file(posts)
+    APIDump.export_json_to_file(posts)
