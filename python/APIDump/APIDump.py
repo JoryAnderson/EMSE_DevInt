024ea26ee4d66defd84f4273183d5b473acd14f8
Jory Anderson <5385311+JoryAnderson@users.noreply.github.com>
2020-11-16 04:05:49 -0800

    name of generated data file can be passed as parameter api config moved from static to function due to import issues during timeout.

--- python/APIDump/APIDump.py
+++ python/APIDump/APIDump.py
@@ -3,21 +3,25 @@ from pathlib import Path
 from stackapi import StackAPI
 import json
 
-QUERY_SIZE = 10
-NUM_OF_QUERIES = 1
+QUERY_SIZE = 100
+NUM_OF_QUERIES = 250
 
-SITE = StackAPI('stackoverflow')
-SITE.key = 'kBC4LfDjAYFLSEFWyrDhdw(( '
-SITE.page_size = QUERY_SIZE
-SITE.max_pages = NUM_OF_QUERIES
+
+def config_api():
+    SITE = StackAPI('stackoverflow')
+    SITE.key = 'kBC4LfDjAYFLSEFWyrDhdw(( '
+    SITE.page_size = QUERY_SIZE
+    SITE.max_pages = NUM_OF_QUERIES
+
+    return SITE
 
 
 def get_questions():
-    return SITE.fetch(filter='withbody', endpoint='questions')
+    return config_api().fetch(filter='withbody', endpoint='questions')
 
 
 def get_answers_for_questions(question_ids):
-    return SITE.fetch(filter='withbody', endpoint='questions/{ids}/answers', ids=question_ids)
+    return config_api().fetch(filter='withbody', endpoint='questions/{ids}/answers', ids=question_ids)
 
 
 def convert_dict_to_json(posts):
@@ -28,12 +32,12 @@ def convert_json_to_dict(json):
     return json.loads(json)
 
 
-def export_json_to_file(posts):
+def export_json_to_file(posts, file_name='data'):
     data = json.dumps(posts, indent=4)
 
     epoch_time = int(time.time())
     Path("data/").mkdir(parents=True, exist_ok=True)
-    f = open("data/data" + str(epoch_time) + ".json", "w")
+    f = open("data/" + file_name + str(epoch_time) + ".json", "w")
     f.write(data)
     f.close()
 
