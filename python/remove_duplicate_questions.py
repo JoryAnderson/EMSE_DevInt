3c550f8236ac079005680e8d334fe6542fd35f31
Jory Anderson <5385311+JoryAnderson@users.noreply.github.com>
2020-11-16 05:07:57 -0800

    Fixed incorrect reference module

--- python/remove_duplicate_questions.py
+++ python/remove_duplicate_questions.py
@@ -3,6 +3,6 @@ from APIDump import JSONReader
 import sys
 
 if __name__ == '__main__':
-    question_data = APIDump.load_json_to_dict(sys.argv[1])
+    question_data = JSONReader.load_json_to_dict(sys.argv[1])
     question_data = JSONReader.remove_duplicate_questions(question_data)
     APIDump.export_json_to_file(question_data, 'nodupe')
