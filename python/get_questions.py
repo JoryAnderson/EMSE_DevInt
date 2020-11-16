70e3a4bb8f2dbccbec642ecc1fc212210b6c86dd
Jory Anderson <5385311+JoryAnderson@users.noreply.github.com>
2020-11-16 05:58:08 -0800

    Added optional command line arguments

--- python/get_questions.py
+++ python/get_questions.py
@@ -1,6 +1,13 @@
-from APIDump import APIDump
+from lib import APIDump
+import sys
 
 if __name__ == '__main__':
-    SITE = APIDump.config_api(100, 250)
-    posts = APIDump.get_questions(SITE)
-    APIDump.export_json_to_file(posts)
+
+    if len(sys.argv) == 3:
+        SITE = APIDump.config_api(sys.argv[1], sys.argv[2])
+        posts = APIDump.get_questions(SITE)
+        APIDump.export_json_to_file(posts)
+    else:
+        SITE = APIDump.config_api(100, 10)
+        posts = APIDump.get_questions(SITE)
+        APIDump.export_json_to_file(posts)
