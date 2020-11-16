9d29815cf46c81af63f9c151127a105f23488d70
Jory Anderson <Jory2554@gmail.com>
2020-11-16 04:06:05 -0800

    Added function to remove duplicates

--- python/APIDump/JSONReader.py
+++ python/APIDump/JSONReader.py
@@ -1,6 +1,6 @@
 import json
 import APIDump
-
+from collections import defaultdict
 
 def load_json_to_dict(file):
     with open(file) as json_file:
@@ -40,7 +40,42 @@ def get_combined_qa_list(question_data, answer_data, question_ids):
     return all_question_answer_text
 
 
+def get_question_indices(questions):
+    index_dict = defaultdict(list)
+
+    for question in range(0, len(questions['items'])):
+        question_id = questions['items'][question]['question_id']
+        index_dict[question_id].append(question)
+
+    return index_dict
+
+
+def remove_duplicate_questions(questions):
+    questions = dict(questions)
+    curr_questions = len(questions['items'])
+
+    index_dict = get_question_indices(question_data)
+    for question_id in index_dict:
+        index_dict[question_id] = index_dict[question_id][1:]
+
+    joined_index_list = []
+    for index in index_dict.values():
+        joined_index_list = joined_index_list + index
+
+    for index in sorted(joined_index_list, reverse=True):
+        del questions['items'][index]
+
+    print("Total deleted questions: " + str(curr_questions - len(questions['items'])))
+    return questions
+
+
 if __name__ == '__main__':
+    # Load Question Data
+    question_data = load_json_to_dict('data/data1605396201.json.json')
+    question_data = remove_duplicate_questions(question_data)
+    APIDump.export_json_to_file(question_data, 'nodupe')
+
+    '''
     # Load Question Data
     question_data = load_json_to_dict('data/small_question_sample.json')
     question_ids = grab_all_question_ids(question_data)
@@ -53,3 +88,4 @@ if __name__ == '__main__':
     for question_id in important_text:
         print(question_id)
         print(important_text.get(question_id))
+    '''
