08ec598e3447c64b1feff09a4803a2f1573d7d7d
Jory Anderson <5385311+JoryAnderson@users.noreply.github.com>
2020-11-16 05:05:03 -0800

    Removed constants, now are optional parameters in config_api Fetch functions are now passed SITE. Fixed reference to question data in JSONReader.py Removed entry functions

--- python/APIDump/JSONReader.py
+++ python/APIDump/JSONReader.py
@@ -1,6 +1,6 @@
 import json
-import APIDump
 from collections import defaultdict
+
 
 def load_json_to_dict(file):
     with open(file) as json_file:
@@ -18,7 +18,6 @@ def grab_all_question_ids(data):
 #   list[0][1] contains the body of the first question
 #   list[0][n] contains body for any answers (of the first question) which may or may not exist.
 def get_combined_qa_list(question_data, answer_data, question_ids):
-
     # {question_id : list}
     all_question_answer_text = {}
 
@@ -54,7 +53,7 @@ def remove_duplicate_questions(questions):
     questions = dict(questions)
     curr_questions = len(questions['items'])
 
-    index_dict = get_question_indices(question_data)
+    index_dict = get_question_indices(questions)
     for question_id in index_dict:
         index_dict[question_id] = index_dict[question_id][1:]
 
@@ -67,25 +66,3 @@ def remove_duplicate_questions(questions):
 
     print("Total deleted questions: " + str(curr_questions - len(questions['items'])))
     return questions
-
-
-if __name__ == '__main__':
-    # Load Question Data
-    question_data = load_json_to_dict('data/data1605396201.json.json')
-    question_data = remove_duplicate_questions(question_data)
-    APIDump.export_json_to_file(question_data, 'nodupe')
-
-    '''
-    # Load Question Data
-    question_data = load_json_to_dict('data/small_question_sample.json')
-    question_ids = grab_all_question_ids(question_data)
-
-    # Get answers using Question Data
-    answer_data = APIDump.get_answers_for_questions(question_ids)
-
-    # Combine question/answer information
-    important_text = get_combined_qa_list(question_data, answer_data, question_ids)
-    for question_id in important_text:
-        print(question_id)
-        print(important_text.get(question_id))
-    '''
