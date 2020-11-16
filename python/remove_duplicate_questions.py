c92680324e6522199ada061cebe1aef2fc1fba6f
Jory Anderson <Jory2554@gmail.com>
2020-11-16 05:05:26 -0800

    Added ad-hoc scripts

---
+++ python/remove_duplicate_questions.py
@@ -0,0 +1,8 @@
+from APIDump import APIDump
+from APIDump import JSONReader
+import sys
+
+if __name__ == '__main__':
+    question_data = APIDump.load_json_to_dict(sys.argv[1])
+    question_data = JSONReader.remove_duplicate_questions(question_data)
+    APIDump.export_json_to_file(question_data, 'nodupe')
