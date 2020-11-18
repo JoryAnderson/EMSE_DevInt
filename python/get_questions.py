3fcab9b6bbee88429d61770da67f1b1cf286524c
Jory Anderson <Jory2554@gmail.com>
2020-11-18 03:29:53 -0800

    Added ability to specify to_date

--- python/get_questions.py
+++ python/get_questions.py
@@ -1,3 +1,6 @@
+import calendar
+import datetime
+
 from lib import APIDump
 import sys
 
@@ -5,9 +8,9 @@ if __name__ == '__main__':
 
     if len(sys.argv) == 3:
         SITE = APIDump.config_api(sys.argv[1], sys.argv[2])
-        posts = APIDump.get_questions(SITE)
+        posts = APIDump.get_questions(SITE, to_date=datetime.datetime(2020, 7, 31))
         APIDump.export_json_to_file(posts)
     else:
-        SITE = APIDump.config_api(100, 10)
-        posts = APIDump.get_questions(SITE)
+        SITE = APIDump.config_api(100, 100)
+        posts = APIDump.get_questions(SITE, to_date=datetime.datetime(2020, 3, 31))
         APIDump.export_json_to_file(posts)
