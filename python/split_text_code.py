7b9aabb05de5440051c083efeb554099330b6bb1
symjamie <sym199512@gmail.com>
2020-11-19 09:39:49 -0800

    Update split_text_code.py

--- python/split_text_code.py
+++ python/split_text_code.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 '''
 Given a JSON file of question posts, split the body in each post
 into text and code block(s), then save title, text, and each code
@@ -12,31 +11,23 @@ import re
 
 PATH_OUT = "data_splitted/"
 
-=======
-# Split body in post into text and code block(s)
-
 import re
 
->>>>>>> 5816c2a05f735bb5e70b183fff184311a65ea352
 # Remove HTML tags in text
 def remove_tags(text):
 	pat = re.compile(r'<.*?>')
 	return pat.sub('', text)
 
-<<<<<<< HEAD
 
-=======
->>>>>>> 5816c2a05f735bb5e70b183fff184311a65ea352
 # Input: the body of a post as a string
 # Output: (a list of code blocks as strings, 
-#		   the body with code blocks removed as a string)
+#          the body with code blocks removed as a string)
 def split(body):
 	pat = re.compile(r"<code>(.+?)<\/code>", flags=re.DOTALL)
 	codes = pat.findall(body) # extraction
 	text = remove_tags(pat.sub('', body)) # removal
 	return text, codes
 
-<<<<<<< HEAD
 
 def split_questions_in_json(data):
 	os.system("mkdir " + PATH_OUT)
@@ -74,16 +65,6 @@ def main(argv):
 		with open(argv[-1]) as f:
 			split_questions_in_json(json.load(f))
 
-
-if __name__ == '__main__':
-	main(sys.argv)
-=======
-def main():
-	example = "<p>I want to write some automation to website. Simply filling in forms and simulating clicks on elements. But for some elements JavaScript function click() doesn't work. For example it is on this page: <a href=\"http://www1.plus.pl/bsm/\" rel=\"noreferrer\">http://www1.plus.pl/bsm/</a> If I do click() on \"Wy\u015blij\" button, nothing happens. It is my command:</p>\n\n<pre><code>document.getElementsByClassName('tab-button')[0].click()\n</code></pre>\n\n<p>What is wrong? Why on other elements this function works?</p>\n"
-	text, code = split(example)
-	print(text)
-	print(code)
-
+	
 if __name__ == '__main__':
 	main()
->>>>>>> 5816c2a05f735bb5e70b183fff184311a65ea352
