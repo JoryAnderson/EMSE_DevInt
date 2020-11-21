3c2e061441c4b0451a7008e9234c75f85fb518cc
Jory Anderson <Jory2554@gmail.com>
2020-11-21 13:17:44 -0800

    Added ability to specify start index and grab next 25k questions or until EOF

--- python/compile_question_answer_text.py
+++ python/compile_question_answer_text.py
@@ -3,9 +3,20 @@ from lib import JSONReader
 import sys
 
 if __name__ == '__main__':
-    # Load Question Data
+    # Load Question Data, all unique
     question_data = JSONReader.load_json_to_dict(sys.argv[1])
     question_ids = JSONReader.grab_all_question_ids(question_data)
+
+    # Grab next 25k questions from starting index
+    start_index = int(sys.argv[2])
+    end_index = min(start_index + 25000, len(question_data['items']))
+
+    # Modify questions to only include specified 25k questions
+    question_data['items'] = question_data['items'][start_index:end_index]
+    question_ids = question_ids[start_index:end_index]
+    print("Indicies: ", start_index, end_index)
+    print("Number of questions from dataset: ", len(question_data['items']))
+    print("Number of questions from question_id list: ", len(question_ids))
 
     # Get answers using Question Data
     SITE = APIDump.config_api()
