89f3cc3de9d39fa0faf0acaee67e9820f727aac5
Jory Anderson <Jory2554@gmail.com>
2020-11-30 23:09:32 -0800

    Pulled stratification and statistics reporting into individual functions Revised answers proportion to get counts of questions and answers from only the JSONs for each user

--- python/rq3_get_stats.py
+++ python/rq3_get_stats.py
@@ -1,6 +1,72 @@
+from collections import defaultdict
+
 import JSONReader
 import matplotlib.pyplot as plt
-import numpy as np
+
+
+def stratify_proportions(proportion_data):
+    stratified_counts = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    for percentage in proportion_data:
+        if percentage == 0:
+            stratified_counts[0] = stratified_counts[0] + 1
+        elif percentage < 0.1:
+            stratified_counts[1] = stratified_counts[1] + 1
+        elif percentage < 0.2:
+            stratified_counts[2] = stratified_counts[2] + 1
+        elif percentage < 0.3:
+            stratified_counts[3] = stratified_counts[3] + 1
+        elif percentage < 0.4:
+            stratified_counts[4] = stratified_counts[4] + 1
+        elif percentage < 0.5:
+            stratified_counts[5] = stratified_counts[5] + 1
+        elif percentage < 0.6:
+            stratified_counts[6] = stratified_counts[6] + 1
+        elif percentage < 0.7:
+            stratified_counts[7] = stratified_counts[7] + 1
+        elif percentage < 0.8:
+            stratified_counts[8] = stratified_counts[8] + 1
+        elif percentage < 0.9:
+            stratified_counts[9] = stratified_counts[9] + 1
+        elif percentage < 1:
+            stratified_counts[10] = stratified_counts[10] + 1
+        elif percentage == 1:
+            stratified_counts[11] = stratified_counts[11] + 1
+    return stratified_counts
+
+
+def stratified_statistics_and_plot(stratified_counts):
+
+    y_labels = ["0%", "1%-9%", "10%-19%", "20%-29%", "30%-39%", "40%-49%", "50%-59%", "60%-69%", "70%-79%", "80%-89%",
+                "90%-99%", "100%"]
+    y_pos = [i for i, _ in enumerate(y_labels)]
+
+    num_of_users_with_answers = 0
+    num_of_users_with_no_answers = stratified_counts[0]
+    less_than_50_percent = num_of_users_with_no_answers
+    more_than_50_percent = 0
+
+    for collection in stratified_counts[1:6:]:
+        num_of_users_with_answers = num_of_users_with_answers + collection
+        less_than_50_percent = less_than_50_percent + collection
+
+    for collection in stratified_counts[6::]:
+        num_of_users_with_answers = num_of_users_with_answers + collection
+        more_than_50_percent = more_than_50_percent + collection
+
+    print("Percentage of users with no answers: " +
+          str(num_of_users_with_no_answers / (num_of_users_with_no_answers + num_of_users_with_answers)))
+    print("Num of no-answerers: " + str(num_of_users_with_no_answers))
+    print("Num of answerers: " + str(num_of_users_with_answers))
+    print("Percentage of users with more answers than questions: "
+          + str(more_than_50_percent / (more_than_50_percent + less_than_50_percent)))
+    print("Less than 50% answers: " + str(less_than_50_percent))
+    print("More than 50% answers: " + str(more_than_50_percent))
+
+    plt.barh(y_pos, stratified_counts)
+    plt.yticks(y_pos, y_labels)
+    plt.xscale("log")
+    plt.xlabel("Number of developers who have asked a question")
+    plt.ylabel("The proportion of answers in each developer's posts")
 
 
 def num_posted_answers(answers_users):
@@ -44,71 +110,39 @@ def posts_as_percentages(users):
         else:
             percentages.append(answer_count / (answer_count + question_count))
 
-    stratified_counts = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    for percentage in percentages:
-        if percentage == 0:
-            stratified_counts[0] = stratified_counts[0] + 1
-        elif percentage < 0.1:
-            stratified_counts[1] = stratified_counts[1] + 1
-        elif percentage < 0.2:
-            stratified_counts[2] = stratified_counts[2] + 1
-        elif percentage < 0.3:
-            stratified_counts[3] = stratified_counts[3] + 1
-        elif percentage < 0.4:
-            stratified_counts[4] = stratified_counts[4] + 1
-        elif percentage < 0.5:
-            stratified_counts[5] = stratified_counts[5] + 1
-        elif percentage < 0.6:
-            stratified_counts[6] = stratified_counts[6] + 1
-        elif percentage < 0.7:
-            stratified_counts[7] = stratified_counts[7] + 1
-        elif percentage < 0.8:
-            stratified_counts[8] = stratified_counts[8] + 1
-        elif percentage < 0.9:
-            stratified_counts[9] = stratified_counts[9] + 1
-        elif percentage < 1:
-            stratified_counts[10] = stratified_counts[10] + 1
-        elif percentage == 1:
-            stratified_counts[11] = stratified_counts[11] + 1
+    stratified_counts = stratify_proportions(percentages)
+    stratified_statistics_and_plot(stratified_counts)
 
-    y_labels = ["0%", "1%-9%", "10%-19%", "20%-29%", "30%-39%", "40%-49%", "50%-59%", "60%-69%", "70%-79%", "80%-89%",
-                "90%-99%", "100%"]
-    y_pos = [i for i, _ in enumerate(y_labels)]
 
-    num_of_users_with_answers = 0
-    num_of_users_with_no_answers = stratified_counts[0]
-    less_than_50_percent = 0
-    more_than_50_percent = 0
+def sampled_answers_proportion(questions_data, answers_data):
 
-    for collection in stratified_counts[1:6:]:
-        num_of_users_with_answers = num_of_users_with_answers + collection
-        less_than_50_percent = less_than_50_percent + collection
+    answer_user_occurrence_map = JSONReader.map_users_to_post(answers_data)
+    questions_user_occurrence_map = JSONReader.map_users_to_post(questions_data)
 
-    for collection in stratified_counts[6::]:
-        num_of_users_with_answers = num_of_users_with_answers + collection
-        more_than_50_percent = more_than_50_percent + collection
+    question_answer_proportions = []
+    for user_id in questions_user_occurrence_map.keys():
+        question_count = len(questions_user_occurrence_map[user_id])
+        answer_count = len(answer_user_occurrence_map[user_id])
+        if (answer_count + question_count) == 0:
+            question_answer_proportions.append(0)
+        else:
+            question_answer_proportions.append(answer_count / (answer_count + question_count))
 
-    print("Percentage of users with no answers: " +
-          str(num_of_users_with_no_answers/(num_of_users_with_no_answers + num_of_users_with_answers)))
-    print("Num of no-answerers: " + str(num_of_users_with_no_answers))
-    print("Num of answerers: " + str(num_of_users_with_answers))
-    print("Percentage of users with more answers than questions: "
-          + str(more_than_50_percent / (more_than_50_percent + less_than_50_percent)))
-    print("Less than 50% answers: " + str(less_than_50_percent))
-    print("More than 50% answers: " + str(more_than_50_percent))
-
-    plt.barh(y_pos, stratified_counts)
-    plt.yticks(y_pos, y_labels)
-    plt.xlabel("Number of developers who have asked a question")
-    plt.ylabel("The proportion of answers in each developer's posts")
+    stratified_counts = stratify_proportions(question_answer_proportions)
+    stratified_statistics_and_plot(stratified_counts)
 
 
 if __name__ == '__main__':
-    users_from_questions = JSONReader.load_json_to_dict('data/users_from_questions.json')
-    users_from_answers = JSONReader.load_json_to_dict('data/users_from_answers.json')
 
-    #plt.subplot(1, 2, 1)
-    num_posted_answers(users_from_answers)
-    #plt.subplot(1, 2, 2)
-    #posts_as_percentages(users_from_questions)
+    # users_from_questions = JSONReader.load_json_to_dict('data/users_from_questions.json')
+    # users_from_answers = JSONReader.load_json_to_dict('data/users_from_answers.json')
+    answers = JSONReader.load_json_to_dict('data/answers_100k_full.json')
+    questions = JSONReader.load_json_to_dict('data/nodupe_100k_from_236k.json')
+
+    sampled_answers_proportion(questions, answers)
+
+    # plt.subplot(1, 2, 1)
+    # num_posted_answers(users_from_answers)
+    # plt.subplot(1, 2, 2)
+    # posts_as_percentages(users_from_questions)
     plt.show()
