1297994ae984a159513132caf200831ac03d9759
symjamie <sym199512@gmail.com>
2020-12-06 14:43:05 -0800

    Update split_text_code.py

--- python/split_text_code.py
+++ python/split_text_code.py
@@ -3,8 +3,8 @@ Given two JSON file for questions and answers in argument:
 	* For each file:
 		* split the body in each post into text and code
 		* remove reserved key words in code
-		* for questions, store (question_id, title, tags, text, code) in a csv file;
-		  for answers, store (question_id, text, code) in another csv file
+		* for questions, store (question_id, user_id, title, tags, text, code) in a csv file;
+		  for answers, store (question_id, user_id(answer-er), text, code) in another csv file
 '''
 
 import sys
@@ -14,10 +14,12 @@ import pandas as pd
 import reserved_key_words
 from lib import JSONReader
 
-OUT_QUESTIONS = "split_questions.csv"
-OUT_ANSWERS = "split_answers.csv"
-DO_QUESTIONS = True
-DO_ANSWERS = False
+IN_QUESTIONS = "../data/questions_100k_full.json"
+IN_ANSWERS = "../data/answers_100k_full_nodup.json"
+OUT_QUESTIONS = "../data/split_questions.csv"
+OUT_ANSWERS = "../data/split_answers.csv"
+DO_QUESTIONS = False
+DO_ANSWERS = True
 
 # Input: the body of a post as a string
 # Output: (a list of code blocks as strings, 
@@ -44,68 +46,85 @@ def process(question_data, answer_data):
 		print("Processing questions")
 
 		qids = []
+		uids = []
 		titles = []
 		tags = []
 		texts = []
 		codes = []
 
 		i = 0
+		dne = 0
 		for question in question_data["items"]:
 			i += 1
 			if i % 1000 == 0:
 				print(i)
 
 			qids += [question["question_id"]]
+
+			user = question["owner"]
+			if user['user_type'] != "does_not_exist":
+				uid = user['user_id']
+			else:
+				uid = None
+				print('\t', question["question_id"], user)
+				dne += 1
+			uids += [uid]
+
 			titles += [question["title"]]
 			tags += [','.join(question["tags"])]
+
 			text, code = split(question["body"])
 			code = pat_key_words.sub('', code) # remove key words
 			texts += [text]
 			codes += [code]
 
-		list_of_tuples = list(zip(qids, titles, tags, texts, codes))
-		df = pd.DataFrame(list_of_tuples, columns=["qid", "title", "tags", "text", "code"])
+		list_of_tuples = list(zip(qids, uids, titles, tags, texts, codes))
+		df = pd.DataFrame(list_of_tuples, columns=["qid", "uid", "title", "tags", "text", "code"])
 		df.to_csv(OUT_QUESTIONS)
+		print("User ID does not exist:", dne)
 
 
 	if DO_ANSWERS:
 		print("Processing answers")
 
-		# load all the data as a dictionary (use a lot of memory)
-		# {question_id : [question_title, question_body, [answers, empty if none]]}
-		all_question_answer_text = JSONReader.get_combined_qa_list(question_data, answer_data)
+		# {question_id : [(user_id, answer_body)]}
+		qid_answers = JSONReader.get_answer_list(question_data, answer_data)
 
 		qids = []
+		uids = []
 		texts = []
 		codes = []
 
 		i = 0
+		dne = 0
 		# for each question
-		for qid, entry in all_question_answer_text.items():
+		for qid, answers in qid_answers.items():
 			i += 1
 			if i % 1000 == 0:
 				print(i)
 
 			# for each answer to the question
-			for ans in entry[2]:
+			for uid, body in answers:
 				qids += [qid]
-				text, code = split(ans)
+				uids += [uid]
+				text, code = split(body)
 				code = pat_key_words.sub('', code) # remove key words
 				texts += [text]
 				codes += [code]
 
-		list_of_tuples = list(zip(qids, texts, codes))
-		df = pd.DataFrame(list_of_tuples, columns=["qid", "text", "code"])
+				if uid == None:
+					dne += 1
+
+		list_of_tuples = list(zip(qids, uids, texts, codes))
+		df = pd.DataFrame(list_of_tuples, columns=["qid", "uid", "text", "code"])
 		df.to_csv(OUT_ANSWERS)
+		print("User ID does not exist:", dne)
 
 
-def main(argv):
-	if len(argv) != 3:
-		print("USAGE: python3 split_text_code.py <questions_JSON_file> <answers_JSON_file>")
-	else:
-		with open(argv[-2]) as f_q, open(argv[-1]) as f_a:
-			process(json.load(f_q), json.load(f_a))
+def main():
+	with open(IN_QUESTIONS) as f_q, open(IN_ANSWERS) as f_a:
+		process(json.load(f_q), json.load(f_a))
 
 	
 if __name__ == '__main__':
-	main(sys.argv)
+	main()
